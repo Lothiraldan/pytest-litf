4e37f6a3f0dbb67075d284c1971e618a69e3f814
Boris Feld <lothiraldan@gmail.com>
2022-05-28 19:47:27 +0200

    Try to get access to the commit workflow runs

--- .github/actions/litf-job-summary/main.py
+++ .github/actions/litf-job-summary/main.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 
+from github import Github
+
 
 def main():
-    print("ENVIRON", os.environ)
+    g = Github(os.environ["INPUT_REPO-TOKEN"])
+    repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
+    workflow_run = repo.get_workflow_run(os.environ["GITHUB_RUN_ID"])
+    print("RUN", workflow_run)
 
 
 if __name__ == "__main__":
