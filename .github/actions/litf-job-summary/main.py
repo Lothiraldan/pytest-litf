b8f9914b1967c70c8742da05f19a337b447bee50
Boris Feld <lothiraldan@gmail.com>
2022-05-29 10:47:45 +0200

    Fix access to workflow run

--- .github/actions/litf-job-summary/main.py
+++ .github/actions/litf-job-summary/main.py
@@ -7,7 +7,7 @@ from github import Github
 def main():
     g = Github(os.environ["INPUT_REPO-TOKEN"])
     repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
-    workflow_run = repo.get_workflow_run(os.environ["GITHUB_RUN_ID"])
+    workflow_run = repo.get_workflow_run(int(os.environ["GITHUB_RUN_ID"]))
     print("RUN", workflow_run)
 
 
