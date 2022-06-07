b43b3992b25dddba165fe8e34dec4903e8201dc9
Boris Feld <lothiraldan@gmail.com>
2022-06-07 21:46:37 +0200

    Try to fix the JS action and speed up execution

--- .github/actions/litf-job-summary-js/index.js
+++ .github/actions/litf-job-summary-js/index.js
@@ -2,29 +2,33 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
 
-try {
-  const name = core.getInput("artifact-name", { required: true });
+async function run(): Promise<void> {
+  try {
+    const name = core.getInput("artifact-name", { required: true });
 
-  const downloadOptions = {
-    createArtifactFolder: false,
-  };
-  const downloadResponse = await artifactClient.downloadArtifact(
-    name,
-    "./litf-outputs.zip",
-    downloadOptions
-  );
+    const downloadOptions = {
+      createArtifactFolder: false,
+    };
+    const downloadResponse = await artifactClient.downloadArtifact(
+      name,
+      "./litf-outputs.zip",
+      downloadOptions
+    );
 
-  fs.readdir(".", function (err, files) {
-    //handling error
-    if (err) {
-      return console.log("Unable to scan directory: " + err);
-    }
-    //listing all files using forEach
-    files.forEach(function (file) {
-      // Do whatever you want to do with the file
-      console.log(file);
+    fs.readdir(".", function (err, files) {
+      //handling error
+      if (err) {
+        return console.log("Unable to scan directory: " + err);
+      }
+      //listing all files using forEach
+      files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file);
+      });
     });
-  });
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
