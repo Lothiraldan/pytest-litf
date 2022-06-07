582a915caf343f56f756a9eabcce2ee4d7d3b2c5
Boris Feld <lothiraldan@gmail.com>
2022-06-07 21:20:51 +0200

    Try to use a JS Github action

---
+++ .github/actions/litf-job-summary-js/index.js
@@ -0,0 +1,30 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+
+try {
+  const name = core.getInput("artifact-name", { required: true });
+
+  const downloadOptions = {
+    createArtifactFolder: false,
+  };
+  const downloadResponse = await artifactClient.downloadArtifact(
+    name,
+    "./litf-outputs.zip",
+    downloadOptions
+  );
+
+  fs.readdir(".", function (err, files) {
+    //handling error
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+      // Do whatever you want to do with the file
+      console.log(file);
+    });
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
